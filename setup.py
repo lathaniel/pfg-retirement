46059e2b449d031e5f5c481b2bc95b6aaa1ccb62
Adam Lathan <lathan.adam@gmail.com>
2020-09-18 15:11:35 -0600

    Changed version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pfg-retirement",
-    version="0.2.1",
+    version="0.2.2",
     packages=['pfg'],
 
     # Project uses reStructuredText, so ensure that the docutils get
