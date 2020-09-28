aae155f10d04a2588c8aa51411f496b431842990
lathaniel <lathan.adam@gmail.com>
2020-09-28 09:30:44 -0600

    Doc Fixings

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pfg-retirement",
-    version="0.2.5",
+    version="0.2.6",
     packages=['pfg'],
 
     # Project uses reStructuredText, so ensure that the docutils get
